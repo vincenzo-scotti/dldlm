Fix output computation issues to ensure same results of original implementation.

src/bin/evaluate_static.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from shutil import copy2
+from shutil import copy2, move
 import logging
 from datetime import datetime
 from argparse import ArgumentParser, Namespace
@@ -107,6 +107,18 @@ def init_environment(config_file_path: str):
     logging.info("Initialisation completed")
 
 
+def clean_environment():
+    # Declare global variables
+    global current_experiment_dir_path
+    # TODO search for closest in time
+    # List files
+    file_list = sorted(f for f in os.listdir() if f.startswith("experiment_"))
+    if len(file_list) > 0:
+        output_file = file_list.pop(-1)
+        move(output_file, os.path.join(current_experiment_dir_path, output_file))
+        logging.info("Cleaning completed")
+
+
 def load_model():
     # Declare global variables
     global tokenizer_configs, model_configs, tokenizer, model
@@ -151,7 +163,7 @@ def evaluate_model():
     # Log start of evaluation
     logging.info(f"Evaluation started - Current date and time {start_time}")
     # Set model in evaluation mode
-    model.eval()
+    model = model.eval()
     logging.info("Model set in evaluation mode")
     # Iterate over data frame rows
     for idx, (_, row) in enumerate(corpus.iterrows()):
@@ -197,7 +209,7 @@ def evaluate_model():
             past_key_values=tuple(
                 (k[:, :, :context_len], v[:, :, :context_len]) for k, v in model_outputs.past_key_values
             ),
-            latent_ids=torch.tensor([[model.config.latent_token_ids[z_posterior]]], dtype=torch.long, device=device),
+            latent_ids=torch.tensor([[model.config.latent_token_ids[z_policy]]], dtype=torch.long, device=device),
             do_context=False,
             do_policy=False,
             do_response_encoding=False,
@@ -216,9 +228,8 @@ def evaluate_model():
             z_posterior, z_policy,
             policy_z_behavior_p, policy_z_empathy_p,
             behaviour_expected_elicited_sent_rew, behaviour_expected_elicited_resp_len_rew,
-            policy_expected_elicited_sent_rew, behaviour_expected_elicited_resp_len_rew
+            policy_expected_elicited_sent_rew, policy_expected_elicited_resp_len_rew
         ])
-
         # Log step completion
         logging.debug(f"Evaluation step {idx + 1} of {n_elems} completed")
     # Close evaluation
@@ -244,6 +255,8 @@ def main(args: Namespace):
     load_data()
     # Carry on evaluation process
     evaluate_model()
+
+    clean_environment()
 
     return 0
 

src/model/dldlm/modeling_dldlm.py
@@ -173,7 +173,7 @@ class DLDLMPreTrainedModel(GPT2PreTrainedModel):
             if lm_logits is not None and labels is not None:
                 shift_labels: torch.Tensor = labels[..., 1:].contiguous()
                 shift_logits: torch.Tensor = lm_logits[..., :-1, :].contiguous()
-                tgt_len: torch.Tensor = (shift_labels != self.config.ignore_id).float().sum(1)
+                tgt_len: torch.Tensor = (shift_labels != self.config.ignore_id).float().sum(1) + 1.
                 lm_loss: Optional[torch.Tensor] = F.cross_entropy(
                     shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1), reduction='none'
                 ).view(shift_labels.size()).sum(1)
@@ -264,14 +264,14 @@ class DLDLMPreTrainedModel(GPT2PreTrainedModel):
             loss = lm_loss = latent_kl_div = latent_kl_div_threshold = latent_loss = cls_loss = bow_loss = rew_loss = None
 
         # Compute objective
-        if self.config.rl_weight > 0.0:
+        if self.config.rl_weight > 0.0 and g_return is not None:
             # Cumulative objective
             objective = torch.zeros(1 if reduction else batch_size, device=self.device)
             # Language modeling objective
             if lm_logits is not None and labels is not None:
                 shift_labels: torch.Tensor = labels[..., 1:].contiguous()
-                shift_logits: torch.Tensor = lm_logits[..., :-1].contiguous()
-                tgt_len: torch.Tensor = (shift_labels != self.config.ignore_id).float().sum(1)
+                shift_logits: torch.Tensor = lm_logits[..., :-1, :].contiguous()
+                tgt_len: torch.Tensor = (shift_labels != self.config.ignore_id).float().sum(1) + 1
                 lm_obj: Optional[torch.Tensor] = F.cross_entropy(
                     shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1), reduction='none'
                 ).view(shift_labels.size()).sum(1)
@@ -624,7 +624,7 @@ class DLDLMAllHeadsModel(DLDLMPreTrainedModel):
             policy_hidden_states = policy_hidden_outputs.hidden_states
             policy_attentions = policy_hidden_outputs.attentions
             # Compute P
-            policy_logits = self.posterior_head(policy_last_hidden_state)
+            policy_logits = self.policy_head(policy_last_hidden_state)
         else:
             policy_last_hidden_state = policy_past_key_values = policy_hidden_states = policy_attentions = None
             policy_logits = None