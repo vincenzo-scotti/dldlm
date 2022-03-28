8e447a44a4b053429745da23bbc9acbfe6a8dfe8
vincenzo-scotti <vincenzo.scotti@polimi.it>
2022-03-28 11:44:45 +0200

    Update training scripts.

--- scripts/run_train_rl_dldlm.sh
+++ scripts/run_train_rl_dldlm.sh
@@ -2,9 +2,9 @@
 # Move to repository directory to ensure paths work correctly
 cd $DLDLM
 # Train small model
-python $DLDLM/src/bin/train_dldlm.py --config_file_path $DLDLM/resources/configs/train_dldlm_small_config.yaml > experiment_"$(date '+%Y_%m_%d_%H_%M_%S')".out
+python $DLDLM/src/bin/train_rl_dldlm.py --config_file_path $DLDLM/resources/configs/train_dldlm_small_emp_config.yaml > experiment_"$(date '+%Y_%m_%d_%H_%M_%S')".out
 # Train medium model
-python $DLDLM/src/bin/train_dldlm.py --config_file_path $DLDLM/resources/configs/train_dldlm_medium_config.yaml > experiment_"$(date '+%Y_%m_%d_%H_%M_%S')".out
+python $DLDLM/src/bin/train_rl_dldlm.py --config_file_path $DLDLM/resources/configs/train_dldlm_medium_emp_config.yaml > experiment_"$(date '+%Y_%m_%d_%H_%M_%S')".out
 
 # NOTE to run in background detached:
 # nohup python $DLDLM/path/to/script.py --config_file_path $DLDLM/path/to/config.yaml > experiment_"$(date '+%Y_%m_%d_%H_%M_%S')".out &
