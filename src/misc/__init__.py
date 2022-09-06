371f62c8dfeaedb1776ce9f376e1f011e74c7af0
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2022-09-06 10:25:53 +0200

    Make sentiment analysis available at package level.

--- src/misc/__init__.py
+++ src/misc/__init__.py
@@ -1,6 +1,6 @@
 from .scheduling import LinearLR, BetaCyclicalAnnealer
 from .losses import EvaluationMode, LOSS_EVALUATION_MODE_MAPPING
 from .metrics import get_latent_word_stats, get_traces, get_response_samples, get_latents_count
-from .stats import get_word_counts, get_word_document_counts, tf_idf
+from .stats import get_word_counts, get_word_document_counts, tf_idf, sentiment
 from .visualisation import log_word_stats, log_traces, log_generated_response, log_latents_count
 from .visualisation import plot_word_stats, plot_traces
