6a0d2f6baa61068b84849a630aa0d363b6e9171c
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2022-09-06 10:23:44 +0200

    Add sentiment analysis extraction.

--- src/misc/stats.py
+++ src/misc/stats.py
@@ -4,17 +4,22 @@ from typing import List, Set
 from collections import Counter
 
 import spacy
+from spacytextblob.spacytextblob import SpacyTextBlob
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 nltk.download('stopwords')
 nltk.download('punkt')
+
+spacy_nlp = spacy.load('en_core_web_sm')
+spacy_nlp.add_pipe('spacytextblob')
+
 DATA_SPECIFIC_STOPWORDS = {"like", "know", "yeah", "think", "feel", "going", "mean", "kind", "right", "things", "want",
                            "way", "time", "people", "sort", "thing", "good", "said", "okay", "guess", "maybe", "lot",
                            "oh", "little", "got", "work", "feeling", "actually", "need", }
 PUNCTUATION: Set[str] = set("[!\"#$%&()*+,-./:;<=>?@[]\\^`{|}~_']") | {'...', '``', '\'\'', '--'}
-STOP_WORDS: Set[str] = set(stopwords.words('english')) | spacy.load('en_core_web_sm').Defaults.stop_words | PUNCTUATION
+STOP_WORDS: Set[str] = set(stopwords.words('english')) | spacy_nlp.Defaults.stop_words | PUNCTUATION
 
 
 def get_word_counts(s: str, remove_stopwords: bool = False) -> Counter:
@@ -39,3 +44,20 @@ def tf_idf(word_counts: Counter, word_document_counts: Counter, n_docs: int, min
         word: (word_counts[word] / n_words) * log10(n_docs / word_document_counts[word])
         for word in word_counts if word_document_counts[word] > min
     })
+
+
+def sentiment(sample: str) -> str:
+    # Helper function to convert polarity to categorical sentiment
+    def encode_sentiment(polarity: float) -> str:
+        if polarity < -0.6:
+            return 'Very negative'
+        elif polarity < -0.2:
+            return 'Negative'
+        elif polarity < 0.2:
+            return 'Neutral'
+        elif polarity < 0.6:
+            return 'Positive'
+        else:
+            return 'Very Positive'
+
+    return encode_sentiment(spacy_nlp(sample)._.blob.polarity)
