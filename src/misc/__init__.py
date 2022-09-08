b1953b82de42a86b19aa9131098a3eff2df3eca7
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2022-09-08 14:19:00 +0200

    Add package level import of parameter linear scheduler.

--- src/misc/__init__.py
+++ src/misc/__init__.py
@@ -1,4 +1,4 @@
-from .scheduling import LinearLR, BetaCyclicalAnnealer
+from .scheduling import LinearLR, AlphaLinearScheduler, BetaCyclicalAnnealer
 from .losses import EvaluationMode, LOSS_EVALUATION_MODE_MAPPING
 from .metrics import get_latent_word_stats, get_traces, get_response_samples, get_latents_count
 from .stats import get_word_counts, get_word_document_counts, tf_idf, sentiment
